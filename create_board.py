e38169d98c6f6a37059e11b3e542d8e70d416473
Filip Gibarac <filip.gibarac@gmail.com>
2023-07-15 17:42:00 -0600

    updated and verified pawn movement structure and verification

--- create_board.py
+++ create_board.py
@@ -6,24 +6,22 @@ class game:
     
     def announce_winner(self):
         print("winner is %s" % self.winner)
-            
-def main():
-    game = game(starting_board)
 
 def game_setup():
-    global SQUARES, FILE_MAP, EMPTY, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+    global SQUARES, FILE_MAP, EMPTY, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, VALUE_MAP
+    
     SQUARES = {
-    'a1': 1,  'b1': 2,  'c1': 3,  'd1': 4,  'e1': 5,  'f1': 6,  'g1': 7,  'h1': 8,
-    'a2': 9,  'b2': 10, 'c2': 11, 'd2': 12, 'e2': 13, 'f2': 14, 'g2': 15, 'h2': 16,
-    'a3': 17, 'b3': 18, 'c3': 19, 'd3': 20, 'e3': 21, 'f3': 22, 'g3': 23, 'h3': 24,
-    'a4': 25, 'b4': 26, 'c4': 27, 'd4': 28, 'e4': 29, 'f4': 30, 'g4': 31, 'h4': 32,
-    'a5': 33, 'b5': 34, 'c5': 35, 'd5': 36, 'e5': 37, 'f5': 38, 'g5': 39, 'h5': 40,
-    'a6': 41, 'b6': 42, 'c6': 43, 'd6': 44, 'e6': 45, 'f6': 46, 'g6': 47, 'h6': 48,
-    'a7': 49, 'b7': 50, 'c7': 51, 'd7': 52, 'e7': 53, 'f7': 54, 'g7': 55, 'h7': 56,
-    'a8': 57, 'b8': 58, 'c8': 59, 'd8': 60, 'e8': 61, 'f8': 62, 'g8': 63, 'h8': 64
+    'a1': 0, 'b1': 1, 'c1': 2, 'd1': 3, 'e1': 4, 'f1': 5, 'g1': 6, 'h1': 7,
+    'a2': 8, 'b2': 9, 'c2': 10, 'd2': 11, 'e2': 12, 'f2': 13, 'g2': 14, 'h2': 15,
+    'a3': 16, 'b3': 17, 'c3': 18, 'd3': 19, 'e3': 20, 'f3': 21, 'g3': 22, 'h3': 23,
+    'a4': 24, 'b4': 25, 'c4': 26, 'd4': 27, 'e4': 28, 'f4': 29, 'g4': 30, 'h4': 31,
+    'a5': 32, 'b5': 33, 'c5': 34, 'd5': 35, 'e5': 36, 'f5': 37, 'g5': 38, 'h5': 39,
+    'a6': 40, 'b6': 41, 'c6': 42, 'd6': 43, 'e6': 44, 'f6': 45, 'g6': 46, 'h6': 47,
+    'a7': 48, 'b7': 49, 'c7': 50, 'd7': 51, 'e7': 52, 'f7': 53, 'g7': 54, 'h7': 55,
+    'a8': 56, 'b8': 57, 'c8': 58, 'd8': 59, 'e8': 60, 'f8': 61, 'g8': 62, 'h8': 63
     }
     
-    FILE_MAP = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+    FILE_MAP = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
     # Accessing the numeric code for a square
     #square = 'e4'
     #numeric_code = SQUARES[square]
@@ -42,6 +40,8 @@ def game_setup():
     ROOK = 4
     QUEEN = 5
     KING = 6
+    
+    VALUE_MAP = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 10}
     
     current_player = "white"
     castling_rights = {
@@ -66,56 +66,87 @@ def game_setup():
     ]
     return starting_board, current_player, castling_rights, en_passant_square, half_move_counter, full_move_counter
 
-def print_board(board):
-    for rank in range(8):
-        for file in range(8):
-            piece = board[rank][file]
-            print(piece, end=" ")
-        print()
+# Define rule checks for movements
+def generate_pawn_moves(board, current_square, is_white):
+    
+    legal_moves = []
+    flattened_board = []
+    for sub_array in board:
+        flattened_board.extend(sub_array)
 
-def reset_counters(h, f):
-    h = 0
-    f = 0
+    # Determine the direction of pawn movement based on its color
+    # is_white = True
+    direction = 1 if is_white else -1
 
-# Initialize first game state
-board = game_setup()[0]
-current_player = game_setup()[1]
-castling_rights = game_setup()[2]
-en_passant_square = game_setup()[3]
-half_move_counter = game_setup()[4]
-full_move_counter = game_setup()[5]
+    # Calculate the target square for a single square advance
+    # current_square = 'h7'
+    try: current_square = SQUARES[current_square]
+    except: current_square = current_square
+    
+    if flattened_board[current_square] != PAWN:
+        return EMPTY
+    else:
+    
+        single_square = current_square + 8 * direction
 
-# Create a chessboard
-print("Initial Chessboard:")
-print_board(board)
-print("Current player:", current_player, "\n"
-      "Castling rights:", castling_rights, "\n"
-      "En passant square:", en_passant_square, "\n"
-      "Half-move counter:", half_move_counter, "\n"
-      "Full-move counter:", full_move_counter)
+        # Check if the single square advance is a legal move
+        if flattened_board[single_square] == 0:
+            legal_moves.append(single_square)
 
-# Define variables to track the game state
-# white always starts the game
+            # Calculate the target square for a double square advance from the initial rank
+        double_square = current_square + 16 * direction
+
+        # Check if the double square advance is a legal move
+        if ((is_white and current_square < 32) or (not is_white and current_square >= 32 and current_square <= 55)):
+            if flattened_board[double_square] == 0:
+                legal_moves.append(double_square)
+
+        # Calculate the target squares for diagonal captures
+        capture_squares = [current_square + 7 * direction, current_square + 9 * direction]
+        #capture_squares = 8
+
+        # Check if diagonal captures are legal moves
+        for square in capture_squares:
+            #square = 62
+            if abs(FILE_MAP[next(key for key, value in SQUARES.items() if value == abs(min(square,63)))[0]] - FILE_MAP[next(key for key, value in SQUARES.items() if value == current_square)[0]]) == 1:
+                if square >= 0 and square < 64:
+                    if (is_white and flattened_board[square] * direction > 0) or (not is_white and flattened_board[square] * direction < 0):
+                        legal_moves.append(square)
+        
+        legal_moves_notation = []
+        for i in range(len(legal_moves)):
+            legal_moves_notation.append(next(key for key, value in SQUARES.items() if value == legal_moves[i]))
+
+        return legal_moves_notation
+
+generate_pawn_moves(board, 'a7', True)
+#print_board(board)
 
 # Define rule checks for movements
-def check_move(color, move, piece):
+def piece_movement_rules(move, piece, is_white):
+
+    #taking pieces notation
     if "x" in move:
         source_square = move.replace("x", "")[:2]
         target_square = move.replace("x", "")[2:]
+    #check notation
     elif "+" in move:
         source_square = move.replace("+", "")[:2]
         target_square = move.replace("+", "")[2:]
+    #checkmate notation
     elif "#" in move:
         source_square = move.replace("#", "")[:2]
         target_square = move.replace("#", "")[2:]
+    #promotion notation
     elif "=" in move:
         source_square = move.replace("=", "")[:2]
         target_square = move.replace("=", "")[2:]
+    #regular moves
     else:
         source_square = move[:2]
         target_square = move[2:]
         
-    # Convert algebraic notation to numeric codes
+    # Convert algebraaic notation to numeric codes
     source_file = FILE_MAP[source_square[0]]
     source_rank = int(source_square[1])
         
@@ -142,14 +173,14 @@ def check_move(color, move, piece):
                 result = False
         elif target_file != source_file:
             result = False
-        elif color == "white":
+        elif is_white == "white":
             if target_rank == 4 and source_rank == 2:
                 result = True
             elif target_rank == source_rank + 1:
                 result = True
             else:
                 result = False
-        elif color == "black":
+        elif is_white == "black":
             if target_rank == 5 and source_rank == 7:
                 result = True
             elif target_rank == source_rank - 1:
@@ -199,16 +230,38 @@ def check_move(color, move, piece):
     else: result = False
     return result
 
-def check_board(board, move, piece):
-    return True
+
+def print_board(board):
+    for rank in range(8):
+        for file in range(8):
+            piece = board[rank][file]
+            print(piece, end=" ")
+        print()
+
+def reset_counters(h, f):
+    h = 0
+    f = 0
+
+# Initialize first game state
+board = game_setup()[0]
+current_player = game_setup()[1]
+castling_rights = game_setup()[2]
+en_passant_square = game_setup()[3]
+half_move_counter = game_setup()[4]
+full_move_counter = game_setup()[5]
+
+# Create a chessboard
+print("Initial Chessboard:")
+print_board(board)
+print("Current player:", current_player, "\n"
+      "Castling rights:", castling_rights, "\n"
+      "En passant square:", en_passant_square, "\n"
+      "Half-move counter:", half_move_counter, "\n"
+      "Full-move counter:", full_move_counter)
 
 # Make a move on the chessboard 
 def make_move(move):
     global current_player, castling_rights, en_passant_square, half_move_counter, full_move_counter
-    if "#" in move:
-        print("Checkmate!")
-    elif "+" in move:
-        print("Check!")
 
     # Extract source and target squares from the move
     source_square = move[:2]
@@ -224,7 +277,7 @@ def make_move(move):
 
     # Get the piece at the source square
     piece = board[8-source_rank][source_file]
-    check_move(current_player, move, piece)
+    check_move_structure(current_player, move, piece)
     check_board(board, move, piece)
 
     # Move the piece to the target square
@@ -276,7 +329,7 @@ print("After Move:", move, "\n"
       "Half-move counter:", half_move_counter, "\n"
       "Full-move counter:", full_move_counter)
 
-move = "d8h4#"
+move = "d8h4"
 make_move(move)
 print_board(board)
 print("After Move:", move, "\n"
@@ -284,4 +337,4 @@ print("After Move:", move, "\n"
       "Castling rights:", castling_rights, "\n"
       "En passant square:", en_passant_square, "\n"
       "Half-move counter:", half_move_counter, "\n"
-      "Full-move counter:", full_move_counterwwww)
+      "Full-move counter:", full_move_counter)
